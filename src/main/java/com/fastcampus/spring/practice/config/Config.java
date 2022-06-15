57e4fb8899d6008460742dea7b3344e1786663f1
jay <jay.wekeep@gmail.com>
2022-06-15 09:13:53 +0900

    Config #1 - spring config 설정

---
+++ src/main/java/com/fastcampus/spring/practice/config/Config.java
@@ -0,0 +1,10 @@
+package com.fastcampus.spring.practice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("com.fastcampus.spring.practice")
+@Configuration
+public class Config {
+
+}
