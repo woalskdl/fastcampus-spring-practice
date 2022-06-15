83c209051bd25a5eaada52a0d1d5239127d224be
jay <jay.wekeep@gmail.com>
2022-06-16 08:36:06 +0900

    Spring framework 적용

--- src/main/java/com/fastcampus/spring/practice/config/Config.java
+++ src/main/java/com/fastcampus/spring/practice/config/Config.java
@@ -2,7 +2,9 @@ package com.fastcampus.spring.practice.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @ComponentScan("com.fastcampus.spring.practice")
 @Configuration
 public class Config {
